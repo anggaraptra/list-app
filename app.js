302b7ed06488997494520c6bae937a79b3d4b438
anggaraptra <kadekanggaraptra@gmail.com>
2023-09-25 22:46:17 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'list_app',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Listening at http://localhost:5000');
+});
+
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM users', (error, results) => {
+    res.render('index.ejs', { users: results });
+  });
+});
+
+app.listen(5000);
